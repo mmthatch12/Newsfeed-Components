ce0616b23b8f34d5a405dcf81fc0f878c9b0713e
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-06-19 16:23:53 -0600

    finished stretch goal to have expand change to close and back

--- Article/Article.js
+++ Article/Article.js
@@ -15,6 +15,11 @@ class Article {
   expandArticle() {
     // Using our reference to the domElement, toggle a class to expand or hide the article.
     this.domElement.classList.toggle('article-open')
+    if (this.domElement.classList.contains('article-open')) {
+      this.expandButton.textContent = 'close'
+    } else {
+      this.expandButton.textContent = 'expand'
+    }
   }
 }
 
