998e43614c5789053055142cfb700c4c824b1ada
mmthatch12 <50630435+mmthatch12@users.noreply.github.com>
2019-07-10 15:14:00 -0600

    working on setting up article structure

--- Article/Article.js
+++ Article/Article.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /* This is the data we will be using to create our article components */
 /* Look over this data, then proceed to line 91*/
 const data = [
@@ -91,11 +90,18 @@ const data = [
 
   // Step 1: Create a function that creates a component. You will want your component to look like the template below: 
   
+  const articles = document.querySelector('.articles')
+
   function component(){
     const articleDiv = document.createElement('div');
     const articleTitle = document.createElement('h2');
     const articleP = document.createElement('p');
-    
+    const articleSpan = document.createElement('span');
+
+    articleDiv.appendChild(articleTitle)
+    articleDiv.appendChild(articleP)
+    articleDiv.appendChild(articleSpan)
+
   }
 
   // <div class="article">
@@ -120,41 +126,3 @@ const data = [
   // Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
 
 
-=======
-// Because classes are not hoisted you will need to start your code at the bottom of the page.  Look for the comment "START HERE"
-
-class Article {
-  constructor(domElement) {
-    // assign this.domElement to the passed in domElement
-    this.domElement = domElement
-    // create a reference to the ".expandButton" class. 
-    this.expandButton = domElement.querySelector('.expandButton')
-    // Using your expandButton reference, update the text on your expandButton to say "expand"
-    this.expandButton.textContent = 'expand'
-    // Set a click handler on the expandButton reference, calling the expandArticle method.
-    this.expandButton.addEventListener('click', () => this.expandArticle())
-    console.log('Article', this)
-  }
-  expandArticle() {
-    // Using our reference to the domElement, toggle a class to expand or hide the article.
-    this.domElement.classList.toggle('article-open')
-    if (this.domElement.classList.contains('article-open')) {
-      this.expandButton.textContent = 'close'
-    } else {
-      this.expandButton.textContent = 'expand'
-    }
-  }
-}
-
-// START HERE: 
-
-//- Select all classes named ".article" and assign that value to the articles variable.  
-
-//- With your selection in place, now chain .forEach() on to the articles variable to iterate over the articles NodeList and create a new instance of Article by passing in each article as a parameter to the Article class.
-
-
-
-let articles = document.querySelectorAll('.article')
-console.log(articles)
-articles.forEach(arty => new Article(arty))
->>>>>>> f7ba52504bfa2cecb7021853eb08c3a98ae7fcc9
